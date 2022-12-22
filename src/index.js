786e527a9a13ddbe094993a881508112bf09003c
agerr <agerrrr@poron.me>
2022-12-22 21:28:27 +0200

    text change

--- src/index.js
+++ src/index.js
@@ -29,7 +29,7 @@ readdirSync('./src/commands').forEach(file => {
     const command = require(`./commands/${file}`)
     client.commands.set(command.name, command)
 
-    console.log(`Loading ${file} as ${command.name}`)
+    console.log(`Loaded ${file} as ${command.name}`)
 })
 
 
