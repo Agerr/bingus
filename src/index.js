8b7c4873db7e77696a9785cb248943aa7e7a4e67
agerr <agerrrr@poron.me>
2023-01-03 15:34:32 +0200

    Updated configuration and added template

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const { Client, GatewayIntentBits, Partials, version, Collection } = require('discord.js')
-const { token } = require('../config.json')
+const { token } = require('../config/config.json')
 
 
 // Create client
